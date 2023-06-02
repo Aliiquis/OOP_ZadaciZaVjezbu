30e45c7c23f67acd2621823b02d72d3c7ba4b2a6
Aliiquis <fran.blazevic0@gmail.com>
2023-06-02 16:51:35 +0200

    Added ZZV13

--- src/main/java/fer/oop/zzv13/Main.java
+++ src/main/java/fer/oop/zzv13/Main.java
@@ -5,9 +5,10 @@ import jxl.read.biff.BiffException;
 
 import java.io.*;
 import java.net.URL;
-import java.util.AbstractMap;
-import java.util.Comparator;
-import java.util.Map;
+import java.util.*;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import java.util.zip.ZipFile;
 
 public class Main {
@@ -34,15 +35,59 @@ public class Main {
 
             Workbook wb = Workbook.getWorkbook(is);
             PollingResults pr = new PollingResults(wb);
-            
+
+            System.out.println("--------------- 6. ----------------");
             pr.getMayorResults().entrySet().stream()
-                    .map((e -> new AbstractMap.SimpleEntry<>(e.getKey(), e.getValue().get("TOMISLAV TOMAŠEVIĆ"))))
-                    .sorted(Comparator.comparing(Map.Entry<PollingStation, Integer>::getValue).reversed())
+                    .map((e -> new SimpleEntry<>(e.getKey(), e.getValue().get("TOMISLAV TOMAŠEVIĆ"))))
+                    .sorted(Comparator.comparing(Entry<PollingStation, Integer>::getValue).reversed())
                     .limit(10)
                     .forEach(e -> System.out.println(e.getKey().getId()));
 
+            System.out.println("--------------- 7. ----------------");
+            pr.getMayorResults().entrySet().stream()
+                    .flatMap(e -> {
+                        int sum = e.getValue().values().stream().mapToInt(Integer::intValue).sum();
+                        return e.getValue().entrySet().stream().map(c -> new SimpleEntry<>(c.getKey(), (double) c.getValue() / sum));
+                    })
+                    .collect(Collectors.toMap(Entry::getKey, Entry::getValue, Double::max))
+                    .forEach((k, v) -> System.out.printf("%s: %.2f%%\n", k, v * 100));
+
+            System.out.println("--------------- 8. ----------------");
+            getCandidateVotes(pr.getMayorResults()).forEach((k, v) -> System.out.println(k + " " + v));
+
+            System.out.println("--------------- 9. ----------------");
+            getCandidateVotes(pr.getMayorResults()).entrySet().stream()
+                    .sorted(Comparator.comparing(Entry<String, Integer>::getValue).reversed())
+                    .toList()
+                    .forEach(e -> System.out.println(e.getKey() + " " + e.getValue()));
+
+            System.out.println("--------------- 10. ----------------");
+            int totalVotes = pr.getMayorResults().values().stream()
+                    .mapToInt(e -> e.values().stream().mapToInt(Integer::intValue).sum())
+                    .sum();
+            getCandidateVotes(pr.getMayorResults()).entrySet().stream()
+                    .forEach(e -> System.out.printf("%s: %.2f%%\n", e.getKey(), (double) e.getValue() * 100 / totalVotes));
+
+            System.out.println("--------------- 12. ----------------");
+            getTitlesSortedPerVotes(pr.getMayorResults()).forEach(e -> System.out.println(e.getKey() + " " + e.getValue()));
         } catch (IOException | BiffException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Map<String, Integer> getCandidateVotes(Map<PollingStation, Map<String, Integer>> results) {
+        Map<String, Integer> map = new LinkedHashMap<>();
+        results.values().forEach(e -> e.forEach((k, v) -> map.merge(k, v, Integer::sum)));
+        return map;
+    }
+
+    public static List<Entry<String, Integer>> getTitlesSortedPerVotes(Map<PollingStation, Map<String, Integer>> results) {
+        return results.entrySet().stream()
+                .map(e -> new SimpleEntry<>(e.getKey().getTitle(), e.getValue().values().stream().mapToInt(Integer::intValue).sum()))
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue, Integer::sum))
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparing(Entry<String, Integer>::getValue).reversed())
+                .toList();
     }
 }
